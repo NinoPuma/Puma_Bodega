8eec4410db31b236e259484c94258d72b198f0b1
ELnino <apuma2002@gmail.com>
2025-02-11 19:48:29 +0100

    Creacion de proyectos

---
+++ ios_grupo03/ios_grupo03/ios_grupo03App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_grupo03App.swift
+//  ios_grupo03
+//
+//  Created by Antonino Puma on 11/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_grupo03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios_grupo03/ios_grupo03Tests/ios_grupo03Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ios_grupo03Tests.swift
+//  ios_grupo03Tests
+//
+//  Created by Antonino Puma on 11/2/25.
+//
+
+import Testing
+@testable import ios_grupo03
+
+struct ios_grupo03Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
