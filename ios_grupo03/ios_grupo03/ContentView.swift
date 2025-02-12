d9af4e008145ad5c346f9c1651594c09da462904
ELnino <apuma2002@gmail.com>
2025-02-12 20:08:32 +0100

    Inicio de sesión en proceso ios

--- ios_grupo03/ios_grupo03/ContentView.swift
+++ ios_grupo03/ios_grupo03/ContentView.swift
@@ -8,13 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+	//Instanciación de usuario y contraseña
+	@State var usr: String = ""
+	@State var pwd: String = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+            Image("Logo_proyecto_integrador")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 700, height: 300)
+            
+            
+        	TextField ("Usuario", text: $usr)
+                	.keyboardType(.emailAddress)
+                	.disableAutocorrection(true)
+                	.autocapitalization(.none)
+	                .font(.headline)
+        	        .cornerRadius(6)
+                	.padding(.horizontal, 60)
+	                .foregroundStyle(Color.black)
+	                .onChange(of: usr) { oldValue, newValue in
+	                    print("Username nuevo valor: \(newValue)")
+                	}
+        
+            
+            
+            SecureField("Contraseña", text: $pwd)
+	                .disableAutocorrection(true)
+        	        .autocapitalization(.none)
+                	.font(.headline)
+        	        .cornerRadius(6)
+                	.padding(.horizontal, 60)
+	                .foregroundStyle(Color.black)
+        	        .onChange(of: pwd) { oldValue, newValue in
+                	    print("Contraseña nuevo valor: \(newValue)")
+                	}
+            
+	           }
         .padding()
     }
 }
